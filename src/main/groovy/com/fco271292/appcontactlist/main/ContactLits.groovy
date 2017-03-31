4029d7cf73a55c9d71c69488f5c6cdedcbb9bfba
fco271292 <juan271292@hotmail.com>
2017-03-30 22:58:50 -0600

    Se agrega clase para generar archivo, se ajusta cadena con mensaje de consola

--- src/main/groovy/com/fco271292/appcontactlist/main/ContactLits.groovy
+++ src/main/groovy/com/fco271292/appcontactlist/main/ContactLits.groovy
@@ -1,12 +1,15 @@
 package com.fco271292.appcontactlist.main
 
 import com.fco271292.appcontactlist.services.*
+import com.fco271292.appcontactlist.util.FileManager
 
 class ContactLits {
 
 	static main(args) {
 		Operations operations = new Operations()
 		operations.showOptions()
+		FileManager fileManager = new FileManager()
+		File file = fileManager.generateFile()
 	}
 
 }
--- src/main/groovy/com/fco271292/appcontactlist/services/Operations.groovy
+++ src/main/groovy/com/fco271292/appcontactlist/services/Operations.groovy
@@ -5,13 +5,7 @@ class Operations {
 	void showOptions(){
 		Scanner scanner = new Scanner(System.in)
 		String option = ""
-		String menu = '''Agenda de contactos \nSelecciona una opcion:
-			1. Alta
-			2. Buscar
-			3. Actualizar
-			4. Eliminar
-			0. Salir
-		'''
+		String menu = '''Agenda de contactos \nSelecciona una opcion: \n1. Alta \t2. Buscar \t3. Actualizar \t4. Eliminar \t0. Salir\n'''
 		println menu
 		option = scanner.nextLine()
 		println option
