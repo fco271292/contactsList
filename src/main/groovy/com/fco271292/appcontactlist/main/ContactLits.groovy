6e571b53f7f3371ff1c7479977e4ac30be710786
Francisco Reyes <fco271292@gmail.com>
2017-04-03 15:36:12 -0500

    Se corrije el import por cambio de nombre de una clase

--- src/main/groovy/com/fco271292/appcontactlist/main/ContactLits.groovy
+++ src/main/groovy/com/fco271292/appcontactlist/main/ContactLits.groovy
@@ -1,7 +1,7 @@
 package com.fco271292.appcontactlist.main
 
 import com.fco271292.appcontactlist.services.*
-import com.fco271292.appcontactlist.util.FileManager
+import com.fco271292.appcontactlist.util.FileUtil
 
 class ContactLits {
 
