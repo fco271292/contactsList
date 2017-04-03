2b3d5218ddc1af21ef60519037dffff209d925b2
Francisco Reyes <fco271292@gmail.com>
2017-04-03 15:37:37 -0500

    Se agrega la implementacion del metodo para buscar

--- src/main/groovy/com/fco271292/appcontactlist/services/PhonebookAdministrator.groovy
+++ src/main/groovy/com/fco271292/appcontactlist/services/PhonebookAdministrator.groovy
@@ -1,5 +1,6 @@
 package com.fco271292.appcontactlist.services
 
+import com.fco271292.appcontactlist.domain.Person
 import com.fco271292.appcontactlist.domain.PhoneBook
 
 class PhonebookAdministrator {
@@ -7,14 +8,20 @@ class PhonebookAdministrator {
 	def createContact(File file,PhoneBook phoneBook){
 		phoneBook.each {item->
 			file << item.person.name << "," << item.person.lastName << ","  << item.person.birthDate
-			file << item.email
-			file << item.phoneNumbers
+			file << item.email << "," 
+			file << item.phoneNumbers << "," 
 			file << item.address.street << "," << item.address.colony << "," << item.address.country
 			file << "\n"
 		}
 	}
 	
-	def readContact(){}
+	def readContact(File file,String patternToSearch){
+		List contentFile = file.readLines()
+		def result = contentFile.findAll {line->
+			line.startsWith(patternToSearch)
+		}
+		result
+	}
 	
 	def updateContact(){}
 	
